8ceaf17e8b87430408e60facf2b49708a8195f8f
Langballn <nlangballe@hotmail.com>
2017-10-23 19:13:35 -0400

    initial commit

---
+++ PingPong/Assets/scripts/LauncherController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LauncherController : MonoBehaviour {
+
+    private float speed = 0.05f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Vector3 position = this.transform.position;
+            position.x -= speed;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Vector3 position = this.transform.position;
+            position.x += speed;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Vector3 position = this.transform.position;
+            position.z += speed;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Vector3 position = this.transform.position;
+            position.z -= speed;
+            this.transform.position = position;
+        }
+
+    }
+}
---
+++ PingPong/Assets/scripts/TargetConroller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetConroller : MonoBehaviour {
+
+    private float speed = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Vector3 position = this.transform.position;
+            position.x -= speed;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Vector3 position = this.transform.position;
+            position.x += speed;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Vector3 position = this.transform.position;
+            position.z += speed;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 position = this.transform.position;
+            position.z -= speed;
+            this.transform.position = position;
+        }
+        
+    }
+}
